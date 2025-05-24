b2117b13a7a92ffda634c6355cbcb0a6cc6ea86b
Orstred <Orstred@DESKTOP-ACUVG72>
2025-05-24 12:58:27 +0700

    Добавьте файлы проекта.

---
+++ RedHaven/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace RedHaven
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
