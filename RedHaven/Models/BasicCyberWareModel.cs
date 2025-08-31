c72c4f7aeb6ac39d46d9a1f4ba6f133e41bc552c
signat98 <s.ignat98@yandex.ru>
2025-08-31 21:56:47 +0700

    Добавил общую модель кибер имплантов Добавил мена базовых имплантов Начал делать модель всех имплантов

---
+++ RedHaven/Models/BasicCyberWareModel.cs
@@ -0,0 +1,12 @@
+﻿
+
+using RedHaven.Enums.CyberwareEnums;
+
+namespace RedHaven.Models;
+
+    public class BasicCyberWareModel 
+    {
+        public BasicCyberware NeededBasicCyberware { get; set; }
+        public int NumberSlots { get; set; }
+        
+    }
---
+++ RedHaven/Models/CyberWareModel.cs
@@ -0,0 +1,12 @@
+﻿
+namespace RedHaven.Models
+{
+    public abstract CyberWareModel
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Clinic { get; set; }
+        public int HumanityLoss { get; set; }
+    }
+}
+
